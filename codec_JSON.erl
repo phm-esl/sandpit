34c07e5a2c91b6339dd70c587f99d3f85eab0aef
Peter-Henry Mander <peter.mander@erlang-solutions.com>
2024-02-20 17:35:25 +0000

    Fix defects when encoding empty arrays or objects, when decoding single values; rewrite string escape sequences.

--- codec_JSON.erl
+++ codec_JSON.erl
@@ -29,10 +29,22 @@
 -define(colon,$:).      % 58
 -define(backslash,$\\). % 92
 
+-define(TRUE_STRING,"true").
+-define(FALSE_STRING,"false").
+-define(NULL_STRING,"null").
+
+-define(TRUE_BINARY,<< ?TRUE_STRING >>).
+-define(FALSE_BINARY,<< ?FALSE_STRING >>).
+-define(NULL_BINARY,<< ?NULL_STRING >>).
+
+-define(ATOMIC_VALUES,1).
+% When ATOMIC_VALUES is defined, use existing atoms
+% as values to replace decoded binary values.
+
 -type json_value()
       :: tuple() % array
        | map() % object i.e. hash map of pairs
-       | binary() | list() % UTF8 string.
+       | binary() % UTF8 string.
        | number()
        | boolean()
        | 'null'.
@@ -45,22 +57,29 @@ encode(In) ->
      ; Error -> {error,Error} end.
 
 encode_each(In,Out) ->
-  case In
+  try case In
     of   Array when     is_tuple(Array) -> [      encode_array(In) | Out ]
      ;  Object when      is_map(Object) -> [     encode_object(In) | Out ]
      ;  String when     is_list(String) -> [     encode_string(In) | Out ]
      ;  String when   is_binary(String) -> [     encode_string(In) | Out ]
      ; Integer when is_integer(Integer) -> [ integer_to_binary(In) | Out ]
      ;   Float when     is_float(Float) -> [   float_to_binary(In) | Out ]
-     ;                                _ -> [    encode_literal(In) | Out ] end.
+     ; Literal when    is_atom(Literal) -> [    encode_literal(In) | Out ]
+     % any other types produce literal 'null'.
+     ; _ -> [ ?NULL_BINARY | Out] end
+  % Handle values that fail to encode, emit 'null' instead.
+  catch error:badarg -> [ ?NULL_BINARY | Out] end.
 
-encode_literal(true) -> << "true" >>;
-encode_literal(false) -> << "false" >>;
-encode_literal(null) -> << "null" >>.
+encode_literal(true) -> ?TRUE_BINARY;
+encode_literal(false) -> ?FALSE_BINARY;
+encode_literal(null) -> ?NULL_BINARY;
+encode_literal(Atom) when is_atom(Atom) -> encode_string(Atom).
 
 encode_array(In) ->
-  [ ?comma | Array ] = encode_array(tuple_size(In),In,[ ?close_square ]),
-  [ ?open_square | Array ].
+  Out = encode_array(tuple_size(In),In,[ ?close_square ]),
+  case Out of
+    [ ?comma | Array ] -> [ ?open_square | Array ];
+    [ ?close_square ] -> [ ?open_square, ?close_square ] end. % empty array.
 
 encode_array(N,In,Out) when 0 < N ->
   Val = element(N,In),
@@ -68,8 +87,9 @@ encode_array(N,In,Out) when 0 < N ->
 encode_array(0,_,Out) -> Out.
 
 encode_object(In) ->
-  [ ?comma | Object ] = maps:fold(fun encode_object/3,[ ?close_brace ],In),
-  [ ?open_brace | Object ]. 
+  case maps:fold(fun encode_object/3,[ ?close_brace ],In) of
+    [ ?comma | Object ] -> [ ?open_brace | Object ];
+    [ ?close_brace ] -> [ ?open_brace, ?close_brace ] end. % empty object.
 
 encode_object(Key,Val,Out) ->
   [ ?comma, encode_string(Key), ?colon, encode(Val) | Out ].
@@ -80,33 +100,27 @@ encode_string(In) when is_list(In) ->
   unicode:characters_to_binary([ ?quote, escape_string(In), ?quote ]).
 
 escape_string([]) -> [];
-escape_string([?quote|Rest]) ->
-  [?backslash,?quote|escape_string(Rest)];
-escape_string([?backslash|Rest]) ->
-  [?backslash,?backslash|escape_string(Rest)];
-escape_string([?slash|Rest]) ->
-  [?backslash,?slash|escape_string(Rest)];
-escape_string([?backspace|Rest]) ->
-  [?backslash,$b|escape_string(Rest)];
-escape_string([?formfeed|Rest]) ->
-  [?backslash,$f|escape_string(Rest)];
-escape_string([?newline|Rest]) ->
-  [?backslash,$n|escape_string(Rest)];
-escape_string([?return|Rest]) ->
-  [?backslash,$r|escape_string(Rest)];
-escape_string([?tab|Rest]) ->
-  [?backslash,$t|escape_string(Rest)];
-escape_string([Ctrl|Rest]) when 0 =< Ctrl, Ctrl < ?space ->
-  << Hi:4, Lo:4 >> = << Ctrl >>,
-  [ ?backslash, $u, $0, $0, hex(Hi), hex(Lo) | escape_string(Rest)];
-escape_string([Each|Rest]) -> [Each|escape_string(Rest)].
+escape_string([In|Rest]) ->
+  case In
+    of     ?quote -> [?backslash,    ?quote|escape_string(Rest)]
+     ; ?backslash -> [?backslash,?backslash|escape_string(Rest)]
+     ; ?backspace -> [?backslash,        $b|escape_string(Rest)]
+     ;  ?formfeed -> [?backslash,        $f|escape_string(Rest)]
+     ;   ?newline -> [?backslash,        $n|escape_string(Rest)]
+     ;    ?return -> [?backslash,        $r|escape_string(Rest)]
+     ;       ?tab -> [?backslash,        $t|escape_string(Rest)]
+     ; Ctrl when 0 =< Ctrl, Ctrl < ?space
+       -> << Hi:4, Lo:4 >> = << Ctrl >>
+        , [ ?backslash, $u, $0, $0, hex(Hi), hex(Lo) | escape_string(Rest)]
+     ; _ -> [In|escape_string(Rest)] end.
 
 hex(N) when 0 =< N, N < 10 -> N + $0;
 hex(N) when 10 =< N, N < 16 -> N - 10 + $a.
 
-
 decode(Binary) ->
-  decode(0,Binary).
+  case decode(0,Binary)
+    of {Value,Pos} when is_binary(Value) -> {atomic_value(Value),Pos}
+     ; Out -> Out end.
 
 decode(Pos,Binary) when is_binary(Binary) ->
   << Char >> = binary_part(Binary,Pos,1),
@@ -138,34 +152,28 @@ decode_string(Pos,Len,Binary) ->
        -> decode_string(Pos,Len + 1,Binary) end.
 
 restore_string(Binary) when is_binary(Binary) ->
-  String = unicode:characters_to_list(Binary),
-  restore_string(String);
+  case binary:split(Binary,<< ?backslash >>,[global])
+    of [Out] -> Out
+     ; [Head|Split]
+       -> unicode:characters_to_binary([Head|restore_part(Split)]) end.
 
-restore_string([]) -> [];
-restore_string([?backslash,?quote|Rest]) ->
-  [?quote|restore_string(Rest)];
-restore_string([?backslash,?backslash|Rest]) ->
-  [?backslash|restore_string(Rest)];
-restore_string([?backslash,?slash|Rest]) ->
-  [?slash|restore_string(Rest)];
-restore_string([?backslash,$b|Rest]) ->
-  [?backspace|restore_string(Rest)];
-restore_string([?backslash,$f|Rest]) ->
-  [?formfeed|restore_string(Rest)];
-restore_string([?backslash,$n|Rest]) ->
-  [?newline|restore_string(Rest)];
-restore_string([?backslash,$r|Rest]) ->
-  [?return|restore_string(Rest)];
-restore_string([?backslash,$t|Rest]) ->
-  [?tab|restore_string(Rest)];
-restore_string([?backslash,$u,A,B,C,D|Rest]) ->
-  E = int4(A),
-  F = int4(B),
-  G = int4(C),
-  H = int4(D),
-  << Unicode:16 >> = << E:4,F:4,G:4,H:4 >>,
-  [Unicode|restore_string(Rest)];
-restore_string([Each|Rest]) -> [Each|restore_string(Rest)].
+restore_part([]) -> [];
+restore_part([In|Rest]) ->
+  Out = case In
+    of << $b, Tail/binary >> -> [ ?backspace, Tail ]
+     ; << $f, Tail/binary >> -> [ ?formfeed,  Tail ]
+     ; << $n, Tail/binary >> -> [ ?newline,   Tail ]
+     ; << $r, Tail/binary >> -> [ ?return,    Tail ]
+     ; << $t, Tail/binary >> -> [ ?tab,       Tail ]
+     ; << $u,A,B,C,D, Tail/binary >>
+       -> E = int4(A)
+        , F = int4(B)
+        , G = int4(C)
+        , H = int4(D)
+        , << Unicode:16 >> = << E:4,F:4,G:4,H:4 >>
+        , [ Unicode, Tail ]
+     ; _ -> In end,
+   [Out|restore_part(Rest)].
 
 int4(X) when $0 =< X, X =< $9 -> X - $0;
 int4(X) when $A =< X, X =< $F -> 10 + X - $A;
@@ -178,10 +186,13 @@ decode_array(Pos,Binary,Out) ->
   case decode(Pos,Binary)
     of {end_array,Done} -> {list_to_tuple(lists:reverse(Out)),Done}
      ; {value_separator,Next} -> decode_array(Next,Binary,Out)
+     ; {V,Done} when is_binary(V)
+       -> Value = atomic_value(V)
+        , decode_array(Done,Binary,[Value|Out])
      ; {Value,Done}
        when Value =:= null
           ; is_boolean(Value)
-          ; is_list(Value)
+          ; is_binary(Value)
           ; is_map(Value)
           ; is_tuple(Value)
           ; is_number(Value)
@@ -194,38 +205,58 @@ decode_object(Pos,Binary,Out) ->
   case decode(Pos,Binary)
     of {end_object,Done} -> {Out,Done}
      ; {value_separator,Next} -> decode_object(Next,Binary,Out)
-     ; {Name,Colon} when is_list(Name)
-       -> {name_separator,Next} = decode(Colon,Binary)
+     ; {N,Colon} when is_binary(N)
+       -> Name = atomic_name(N)
+        , {name_separator,Next} = decode(Colon,Binary)
         , case decode(Next,Binary)
-            of {Value,Done}
+            of {V,Done} when is_binary(V)
+               -> Value = atomic_value(V)
+                , decode_object(Done,Binary,Out#{ Name => Value })
+             ; {Value,Done}
                when Value =:= null
                   ; is_boolean(Value)
-                  ; is_list(Value)
                   ; is_map(Value)
                   ; is_tuple(Value)
                   ; is_number(Value)
                -> decode_object(Done,Binary,Out#{ Name => Value }) end end.
+
+atomic_name(Name) when is_binary(Name) -> atomic(Name).
+
+-ifdef(ATOMIC_VALUES).
+atomic_value(Value) when is_binary(Value), size(Value) > 0 -> atomic(Value);
+atomic_value(Value) -> Value.
+-else.
+atomic_value(Value) -> Value.
+-endif.
+
+atomic(Bin) when is_binary(Bin) ->
+  try erlang:binary_to_existing_atom(Bin)
+  catch error:badarg -> Bin end.
 
 negative({Number,Pos}) when is_number(Number) -> {-Number,Pos}.
 
 decode_number(Pos,Binary) ->
   decode_number(Pos,0,Binary).
 
-decode_number(Pos,Len,Binary) ->
+decode_number(Pos,Len,Binary) when Pos + Len < size(Binary) ->
   case binary_part(Binary,Pos + Len,1)
     of << Digit >> when $0 =< Digit, Digit =< $9
        -> decode_number(Pos,Len + 1,Binary)
      ; << ?decimal_point >>
        -> decode_float(Pos,Len + 1,Binary)
-     ; _ -> {binary_to_integer(binary_part(Binary,Pos,Len)),Pos + Len} end.
+     ; _ -> {binary_to_integer(binary_part(Binary,Pos,Len)),Pos + Len} end;
+decode_number(Pos,Len,Binary) ->
+  {binary_to_integer(binary_part(Binary,Pos,Len)),Pos + Len}.
 
-decode_float(Pos,Len,Binary) ->
+decode_float(Pos,Len,Binary) when Pos + Len < size(Binary) ->
   case binary_part(Binary,Pos + Len,1)
     of << Digit >> when $0 =< Digit, Digit =< $9
        -> decode_float(Pos,Len + 1,Binary)
      ; << Exp >> when Exp =:= $e; Exp =:= $E
        -> decode_exponent(Pos,Len + 1,Binary)
-     ; _ -> {binary_to_float(binary_part(Binary,Pos,Len)),Pos + Len} end.
+     ; _ -> {binary_to_float(binary_part(Binary,Pos,Len)),Pos + Len} end;
+decode_float(Pos,Len,Binary) ->
+  {binary_to_float(binary_part(Binary,Pos,Len)),Pos + Len}.
 
 decode_exponent(Pos,Len,Binary) ->
   case binary_part(Binary,Pos + Len,1)
@@ -234,42 +265,78 @@ decode_exponent(Pos,Len,Binary) ->
      ; << Sign >> when Sign =:= ?minus; Sign =:= ?plus
        -> decode_exponent_digits(Pos,Len + 1,Binary) end.
 
-decode_exponent_digits(Pos,Len,Binary) ->
+decode_exponent_digits(Pos,Len,Binary) when Pos + Len < size(Binary) ->
   case binary_part(Binary,Pos + Len,1)
     of << Digit >> when $0 =< Digit, Digit =< $9
        -> decode_exponent_digits(Pos,Len + 1,Binary)
-     ; _ -> {binary_to_float(binary_part(Binary,Pos,Len)),Pos + Len} end.
-
+     ; _ -> {binary_to_float(binary_part(Binary,Pos,Len)),Pos + Len} end;
+decode_exponent_digits(Pos,Len,Binary) ->
+  {binary_to_float(binary_part(Binary,Pos,Len)),Pos + Len}.
 
 decode_literal(Pos,Binary) ->
   case Binary
-    of << _:Pos/binary, "true", _/binary >>
+    of << _:Pos/binary, ?TRUE_STRING, _/binary >>
        -> {'true',Pos + 4}
-     ; << _:Pos/binary, "false", _/binary >>
+     ; << _:Pos/binary, ?FALSE_STRING, _/binary >>
        -> {'false',Pos + 5}
-     ; << _:Pos/binary, "null", _/binary >>
+     ; << _:Pos/binary, ?NULL_STRING, _/binary >>
        -> {'null',Pos + 4} end.
 
 
 test() ->
-  JSON =
-  << "[{\"文字化けabc..åäöé𝐀\\r\\n\\u001b\":{\"mojibake\":false}"
-      ",\"integer\":123456"
-      ",\"float\":1.23456789000000003637e+23"
-      ",\"12\":[true,false,null]}"
-     ",\"More data...\""
-     ",999999]"/utf8 >>,
-  Len = byte_size(JSON),
+  test_encode(),
+  test_decode().
 
-  In = 
-    { #{ "文字化けabc..åäöé𝐀\r\n\e" => #{ "mojibake" => false }
-       , "12" => {true,false,null}
-       , "integer" => 123456
-       , "float" => 1.234567890e23 }
-    , "More data..."
-    , 999999 },
-  Out = encode(In),
-  Bin = io_lib:write(Out),
+test_encode() ->
+  Tests
+   = [ {<< $",$" >>,""}
+     , {<< "[]" >>,{}}
+     , {<< "{}" >>,#{}}
+     , {<< "true" >>,true}
+     , { << "[{\"文字化けabc..åäöé𝐀\\r\\n\\u001b\"" % Unicode sequence of octets
+                ":{\"mojibake\":false}"
+              ",\"integer\":123456"
+              ",\"float\":1.23456789000000003637e+23"
+              ",\"12\":[true,false,null]}"
+             ",\"More data...\""
+             ",999999]"/utf8 >>
+       , { #{ "文字化けabc..åäöé𝐀\r\n\e" % list of Unicode characters, not octets!
+              => #{ "mojibake" => false }
+            , "12" => {true,false,null}
+            , "integer" => 123456
+            , "float" => 1.234567890e23 }
+         , "More data..."
+         , 999999 } } ],
+  Failures = lists:dropwhile(fun({A,B}) -> A =:= encode(B) end,Tests),
+  [] = [ {A,encode(B)} || {A,B} <- Failures ].
 
-  io:put_chars(["Bin = ",Bin,$\n,"Out = ",Out,$\n,"test passed: "]),
-  JSON =:= Out andalso {In,Len} =:= decode(Out).
+test_decode() ->
+  Tests
+   = [ {<<>>,<< $",$" >>}
+     , {true,<< "true" >>}
+     , {false,<< "false" >>}
+     , {null,<< "null" >>}
+     , {{},<< "[]" >>}
+     , {#{},<< "{}" >>}
+     , {<< "abc€123\n"/utf8 >>,<< "\"abc\\u20ac123\\n\"" >>}
+     , {-1.23e-45,<< "-1.23e-45" >>}
+     , {undefined,<< "\"undefined\"" >>}
+     , {{undefined},<< "[\"undefined\"]" >>}
+     , { { #{ << "文字化けabc..åäöé𝐀\r\n\e"/utf8 >>
+              => #{ << "mojibake" >> => false }
+            , << "12" >> => {true,false,null}
+            , integer => 123456
+            , float => 1.234567890e23 }
+         , << "More data..." >>
+         , 999999 }
+       , << "[{\"文字化けabc..åäöé𝐀\\r\\n\\u001b\""
+                ":{\"mojibake\":false}"
+              ",\"integer\":123456"
+              ",\"float\":1.23456789000000003637e+23"
+              ",\"12\":[true,false,null]}"
+             ",\"More data...\""
+             ",999999]"/utf8 >> } ],
+  Failures = lists:dropwhile(
+    fun({A,B}) -> {A,size(B)} =:= decode(B) end,
+    Tests),
+  [] = [ {A,decode(B)} || {A,B} <- Failures ].

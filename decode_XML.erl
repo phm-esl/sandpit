29115a9121d062d36052345114050e0349975856
Peter-Henry Mander <peter.mander@erlang-solutions.com>
2024-04-29 17:23:52 +0100

    Fit the decoder closer to the grammar definition.

--- decode_XML.erl
+++ decode_XML.erl
@@ -1,7 +1,9 @@
 -module(decode_XML).
 
 -export([
-  decode/1,
+  document/1]).
+
+-export([
   test/0,
   test_fragments/0 ]).
 
@@ -28,8 +30,8 @@
 -define(comment_start,"<!--").
 -define(comment_end,"-->").
 
--define(prolog_start,"<?").
--define(prolog_end,"?>").
+-define(processsing_instruction_start,"<?").
+-define(processsing_instruction_end,"?>").
 
 -define(declaration_start,"<!").
 -define(declaration_end,">").
@@ -39,35 +41,58 @@
     attributes = #{},
     content = [] }).
 
-decode(In) when is_binary(In) ->
-  Start = fun (Tail) -> decode(Tail,[]) end,
-  white_space(In,Start).
+-define(dbg(F,A),io:format("~p:~p "++F,[?FUNCTION_NAME,?LINE|A])).
 
+%%%
+%%%   Parser that hews closely to the grammer as specified in
+%%%   https://www.w3.org/TR/xml/
+%%%   All XML documents begin with the prolog. No leading spaces.
+%%%
+document(<< ?processsing_instruction_start, Tail/binary >>) ->
+  process_instruct(Tail,fun prolog/2).
 
-decode(In,Done) ->
-  case In of
-    << >> when is_function(Done) ->
-      fun (More) when is_binary(More) ->
-        decode(More,Done) end;
-    << >> -> Done;
-    << ?CDATA_start, Tail/binary >> ->
-      cdata_section(Tail,Done);
-    << ?comment_start, Tail/binary >> ->
-      comment(Tail,Done);
-    << ?prolog_start, Tail/binary >> ->
-      prolog(Tail,Done);
-    << ?declaration_start, Tail/binary >> ->
-      declaration(Tail,Done);
-    << ?ampersand, Tail/binary >> ->
-      entity_reference(Tail,Done);
-    << ?percent, Tail/binary >> ->
-      param_reference(Tail,Done); % example found in DTD
+prolog(Bin,{process_instruct,<< "xml", _/binary>>=PI}) ->
+  Next = fun(T) -> doctypedecl(T,{prolog,PI}) end,
+  white_space(Bin,Next).
+
+%%%
+%%%   Either one or none of doctypedecl, followed by misc.
+%%%
+doctypedecl(Bin,Prolog) ->
+  Done = fun (I,O) -> doctypedecl_end(I,O) end,
+  case Bin of
+    << "<!DOCTYPE", Tail/binary >> ->
+      Next = fun (T,{declaration,_}=Decl) ->
+        misc_space(T,[Decl,Prolog],Done) end,
+      declaration(Tail,Next);
+    _ -> misc_space(Bin,[Prolog],Done) end.
+
+doctypedecl_end(Bin,Out) ->
+  Done = fun (<< >>,Result) -> lists:reverse(Result) end,
+  Misc = fun (T,Element) -> misc_space(T,[Element|Out],Done) end,
+  case Bin of
+    << >> -> Out;
     << ?less_than, Tail/binary >> when Tail /= << >> ->
-      Next = fun(T) -> start_tag_name(T,Done) end,
+      Next = fun(T) -> start_tag_name(T,Misc) end,
       white_space(Tail,Next);
     _ ->
       fun (More) when is_binary(More) ->
-        decode(<< In/binary, More/binary >>,Done) end end.
+        doctypedecl_end(<< Bin/binary, More/binary >>,Out) end end.
+
+%%%
+%%%   Only exists at the document top level.
+%%%
+misc_space(Bin,Out,Done) ->
+  white_space(Bin,fun (T) -> misc(T,Out,Done) end).
+
+misc(Bin,Out,Done) ->
+  Again = fun(I,O) -> misc_space(I,[O|Out],Done) end,
+  case Bin of
+    << ?processsing_instruction_start, Tail/binary >> ->
+      process_instruct(Tail,Again);
+    << ?comment_start, Tail/binary >> ->
+      comment(Tail,Again);
+    _ -> Done(Bin,Out) end.
 
 
 cdata_section(In,Done) -> cdata_section(In,0,Done).
@@ -83,36 +108,74 @@ cdata_section(In,Pos,Done) ->
     _ -> cdata_section(In,Pos + 1,Done) end.
 
 
-comment(In,Done) -> comment(In,0,Done).
+comment(In,Done) when is_function(Done)  -> comment(In,0,Done).
 
 comment(In,Pos,Done) ->
   case In of
     << _:Pos/binary >> ->
+      ?dbg("",[]),
       fun (More) when is_binary(More) ->
         comment(<< In/binary, More/binary >>,Pos,Done) end;
     << Comment:Pos/binary, ?comment_end, Tail/binary >> ->
       Out = {comment,Comment},
-      result(Tail,Out,Done);
+      Done(Tail,Out);
     << _:Pos/binary, ?hyphen,B, _/binary >> when B /= ?hyphen ->
       comment(In,Pos + 1,Done); % double-hyphen MUST NOT occur within comments
     << _:Pos/binary, A, _/binary >> when A /= $- ->
       comment(In,Pos + 1,Done) end.
 
 
-prolog(In,Done) -> prolog(In,0,Done).
+process_instruct(In,Done) when is_function(Done) ->
+  process_instruct(In,0,Done).
 
-prolog(In,Pos,Done) ->
+process_instruct(In,Pos,Done) ->
   Resume = fun (More) when is_binary(More) ->
-        prolog(<< In/binary, More/binary >>,Pos,Done) end,
+        process_instruct(<< In/binary, More/binary >>,Pos,Done) end,
   case In of
     << _:Pos/binary >> -> Resume;
     << _:Pos/binary, $? >> -> Resume;
-    << Prolog:Pos/binary, ?prolog_end, Tail/binary >> ->
-      Out = {prolog,Prolog},
-      result(Tail,Out,Done);
-    _ -> prolog(In,Pos + 1,Done) end.
+    << Prolog:Pos/binary, ?processsing_instruction_end, Tail/binary >> ->
+      Out = {process_instruct,Prolog},
+      Done(Tail,Out);
+    _ -> process_instruct(In,Pos + 1,Done) end.
 
 
+%%%
+%%%   doctypedecl
+%%%   ::= '<!DOCTYPE' S Name (S ExternalID)? S? ('[' intSubset ']' S?)? '>'
+%%%
+%%%   ExternalID
+%%%   ::= 'SYSTEM' S SystemLiteral
+%%%     | 'PUBLIC' S PubidLiteral S SystemLiteral
+%%%
+%%%   SystemLiteral
+%%%   ::= ('"' [^"]* '"')
+%%%     | ("'" [^']* "'")
+%%%
+%%%   PubidLiteral
+%%%   ::= '"' PubidChar* '"'
+%%%     | "'" (PubidChar - "'")* "'"
+%%%   PubidChar
+%%%   ::= #x20
+%%%     | #xD
+%%%     | #xA
+%%%     | [a-zA-Z0-9]
+%%%     | [-'()+,./:=?;!*#@$_%]
+%%%
+%%%   intSubset   ::=   (markupdecl | DeclSep)*
+%%%
+%%%   markupdecl
+%%%   ::=   elementdecl
+%%%     | AttlistDecl
+%%%     | EntityDecl
+%%%     | NotationDecl
+%%%     | PI
+%%%     | Comment
+%%%
+%%%   DeclSep   ::=   PEReference | S
+%%%
+%%%   PEReference   ::=   '%' Name ';'
+%%%
 declaration(In,Done) -> declaration(In,0,Done).
 
 declaration(In,Pos,Done) ->
@@ -128,8 +191,9 @@ declaration(In,Pos,Done) ->
 entity_reference(In,Done) ->
   reference(In,0,entity_ref,Done).
 
-param_reference(In,Done) ->
-  reference(In,0,param_ref,Done).
+%%%   PEReference   ::=   '%' Name ';'
+%param_entity_reference(In,Done) ->
+%  reference(In,0,param_ref,Done).
 
 reference(In,Pos,Type,Done) ->
   case In of
@@ -141,7 +205,7 @@ reference(In,Pos,Type,Done) ->
     _ -> reference(In,Pos + 1,Type,Done) end.
 
 
-start_tag_name(In,Done) ->
+start_tag_name(In,Done) when is_function(Done) ->
   Next = fun (Tail,Elem_name) -> attr_name(Tail,Elem_name,#{},Done) end,
   token(In,0,Next).
 
@@ -178,6 +242,13 @@ quote(In,Pos,Quote,Done) ->
     _ -> quote(In,Pos + 1,Quote,Done) end.
 
 
+
+%%%   content
+%%%   ::= CharData? ( ( element
+%%%                   | Reference
+%%%                   | CDSect
+%%%                   | PI
+%%%                   | Comment ) CharData? )*
 content(In,Pos,Out,Done) ->
   Content = Out#element.content,
   case In of
@@ -197,8 +268,15 @@ content(In,Pos,Out,Done) ->
           end_tag(Rest,Update,Done);
         << ?ampersand, Rest/binary >> ->
           entity_reference(Rest,Next);
-        << ?less_than, _/binary >> ->
-          decode(Tail,Next);
+        << ?CDATA_start, Rest/binary >> ->
+          cdata_section(Rest,Next);
+        << ?comment_start, Rest/binary >> ->
+          comment(Rest,Next);
+        << ?processsing_instruction_start, Rest/binary >> ->
+          process_instruct(Rest,Next);
+        << ?less_than, Rest/binary >> when Rest /= << >> ->
+          Element = fun(T) -> start_tag_name(T,Next) end,
+          white_space(Rest,Element);
         _ -> content(In,Pos + 1,Out,Done) end end.
 
 
@@ -244,38 +322,37 @@ white_space(In,Done) ->
     _ -> Done(In) end.
 
 
-result(In,Out,Done) when is_function(Done) -> Done(In,Out);
-result(In,Out,Done) ->
-  Next = fun (T) -> [Out|decode(T,Done)] end,
-  white_space(In,Next).
+result(In,Out,Done) when is_function(Done) -> Done(In,Out).
 
 
 test_cases() ->
-  [ %{ << "<none/> " >>, [] },
-    { << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" >>,
-      [{prolog,<<"xml version=\"1.0\" encoding=\"UTF-8\" ">>}] },
-    { << "
+  [ { << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" >>,
+      [ {prolog,<<"xml version=\"1.0\" encoding=\"UTF-8\" ">>} ] }
+  | test_top_level() ].
 
-  <!-- Some sort of-
-  commentary... --> <![CDATA[<![CDATA[]]]]><![CDATA[>]]> < element abc=\"123\" def='987' >
- something... < inside /><deeper>bottom &amp; depth</deeper></ element >
-< another empty='true' />
+test_top_level() ->
+  [ { << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+         "     <!--  commentary with spaces  -->"
+         "    <? something or other... ?>"
+         "   <notMT> some text <MT/> more waffle </notMT>"
+         "   <!-- trailing -->"
+         "    <? proc instr ?>"
+         "      "
+      >>,
+      [ {prolog,<<"xml version=\"1.0\" encoding=\"UTF-8\" ">>}
+      , {comment,<< "  commentary with spaces  " >>}
+      , {process_instruct,<< " something or other... " >>}
+      , { element,<<"notMT">>,#{},
+          [ <<" some text ">>
+          , {element,<<"MT">>,#{},empty}
+          , <<" more waffle ">> ] }
+      , {comment,<<" trailing ">>}
+      , {process_instruct,<< " proc instr " >>}
+      ] }
+    | test_SoapXML() ].
 
-" >>, [ {comment,<< " Some sort of-
-  commentary... " >>},
-      {'CDATA',<<"<![CDATA[]]">>},
-      {'CDATA',<<">">>},
-      {element,<< "element" >>,
-       #{<<"abc">> => <<"123">>,<<"def">> => <<"987">>},
-       [ << "\n something... " >>,
-         {element,<<"inside">>,#{},empty},
-         {element,<<"deeper">>,#{},[
-           <<"bottom ">>,
-           {entity_ref,<<"amp">>},
-           <<" depth">>]} ]},
-      {element,<< "another" >>,
-       #{<<"empty">> => <<"true">>},empty} ] },
-    {<< "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+test_SoapXML() ->
+  [ {<< "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
            "<soapenv:Envelope"
            " xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\""
            " xmlns:urn=\"urn:headerblock\""
@@ -374,13 +451,15 @@ test_cases() ->
                                 [<<"Value that exists now">>]}]}]}]}]}]}]} ].
 
 test() ->
-  Tests = test_cases(),
+  test(test_cases()).
+
+test(Tests) ->
   Filter = fun filter/1,
   Failures = lists:filter(Filter,Tests),
-  [{In,Good,decode(In)} || {In,Good} <- Failures ].
+  [{In,Good,document(In)} || {In,Good} <- Failures ].
 
 filter({In,Good}) ->
-  Out = decode(In),
+  Out = document(In),
   Out /= Good.
 
 test_fragments() ->
@@ -388,13 +467,13 @@ test_fragments() ->
   lists:filter(fun (T) -> test_frag(T) end, Tests).
 
 test_frag(Test) ->
-  not test_frag(Test,1).
+  not test_frag(Test,2).
 
 test_frag({Bin,Good}=Test,Pos) ->
   case Bin of
     << _:Pos/binary >> -> true;
     << Head:Pos/binary, Tail/binary >> ->
-      Fn = decode(Head),
+      Fn = document(Head),
       Pass = is_function(Fn) andalso Good =:= Fn(Tail) orelse Good =:= Fn,
       if Pass -> test_frag(Test,Pos + 1);
          not is_function(Fn) -> io:format("Fn = ~p.~n",[Fn]), false;

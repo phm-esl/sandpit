b691e749c7b9bbd783fc568a4cf1cc30d0772de7
Peter-Henry Mander <peter.mander@erlang-solutions.com>
2024-05-13 17:35:47 +0100

    Improve data format of XML-path extractions.

--- protocol_tools/src/codec_xml.erl
+++ protocol_tools/src/codec_xml.erl
@@ -539,6 +539,20 @@ filter({In,Good}) ->
   Out /= Good.
 
 test_decode(In) ->
+  %%
+  %%  Extraction paths as nested maps:
+  %%
+  Y = #{ 'Envelope' =>
+         #{ 'Header' => #{},
+            'Body' =>
+              #{ trigger =>
+                 #{ attribute => #{} } } } },
+
+  Elements = extraction_paths(Y),
+
+  %%
+  %%  Extraction paths as nested tuples:
+  %%
   X = {'Envelope',[
         {'Header',[ ]},
         {'Body',[
@@ -553,7 +567,15 @@ test_decode(In) ->
   loop(decode(In),[],Elements).
 
 
-extraction_paths(Paths) ->
+tree_from_map(In) ->
+  maps:fold(
+    fun (K,V,O) -> [{K,tree_from_map(V)}|O] end, [], In).
+
+extraction_paths(Paths) when is_map(Paths) ->
+  case tree_from_map(Paths) of
+    [Tree] -> extraction_paths(Tree);
+    [] -> [] end;
+extraction_paths(Paths) when is_tuple(Paths) ->
   lists:foldl(
     fun (I,O) -> O#{ lists:reverse(I) => [] } end,
     #{},
@@ -562,6 +584,20 @@ extraction_paths(Paths) ->
 unroll({Atom,[]}) -> [[Atom]];
 unroll({Atom,List}) -> [ [Atom|U] || L <- List, U <- unroll(L) ].
 
+
+rollup(In) when is_map(In) ->
+  Rev_keys = maps:fold(fun rev_keys/3,[],In),
+  lists:foldl(fun to_tree/2,#{},Rev_keys).
+
+rev_keys(Key,Value,Out) -> [{lists:reverse(Key),Value}|Out].
+
+to_tree({[Last],Value},Out) when not is_map_key(Last,Out) ->
+  Out#{ Last => Value };
+to_tree({[Head|Tail],Value},Out) when is_map_key(Head,Out)  ->
+  Exist = maps:get(Head,Out),
+  Out#{ Head := to_tree({Tail,Value},Exist) };
+to_tree({[Head|Tail],Value},Out) when not is_map_key(Head,Out)  ->
+  Out#{ Head => to_tree({Tail,Value},#{}) }.
 
 
 loop({Atom,Content,Fn},In,Out) when is_function(Fn) ->
@@ -574,7 +610,7 @@ loop({Atom,Content,Fn},In,Out) when is_function(Fn) ->
      true ->
        loop(Fn(),In,Out) end;
 loop(Decoded,[],Out) ->
-  ?log("Out = ~p.~n",[Out]),
+  ?log("Extracted = ~p.~n",[rollup(Out)]),
   Decoded.
 
 test_fragments() ->
